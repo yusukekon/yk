f5dff0551bd5b764b4e0efe9b44a559659a773f7
yusukekon <kkoonn84@gmail.com>
2013-04-13 13:58:07 +0900

    test.html -> all_tests.html にリネーム

--- bin/run_jstest.sh
+++ bin/run_jstest.sh
@@ -7,7 +7,7 @@ PHANTOMJS_HOME=$BASE_DIR/3rd-party/phantomjs-1.8.1/
 $PHANTOMJS_HOME/bin/phantomjs -v
 
 if [ $# -eq 0 ]; then
-  $PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $BASE_DIR/js/tests.html
+  $PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $BASE_DIR/js/all_tests.html
 else
   $PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $@
 fi
