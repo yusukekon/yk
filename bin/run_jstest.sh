ed30d234717f8343e4bfb701dbbd0fb22f42bbf6
yusukekon <kkoonn84@gmail.com>
2013-04-13 13:57:04 +0900

    run_jstest.sh でテスト対象を省略すると tests.html を走らせるようにした

--- bin/run_jstest.sh
+++ bin/run_jstest.sh
@@ -5,5 +5,9 @@ QUNIT_HOME=$BASE_DIR/3rd-party/qunit/
 PHANTOMJS_HOME=$BASE_DIR/3rd-party/phantomjs-1.8.1/
 
 $PHANTOMJS_HOME/bin/phantomjs -v
-#$PHANTOMJS_HOME/bin/phantomjs $PHANTOMJS_HOME/examples/run-qunit.js $@
-$PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $@
+
+if [ $# -eq 0 ]; then
+  $PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $BASE_DIR/js/tests.html
+else
+  $PHANTOMJS_HOME/bin/phantomjs $QUNIT_HOME/addons/phantomjs/runner.js $@
+fi
